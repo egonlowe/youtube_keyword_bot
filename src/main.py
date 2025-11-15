5d37331260ef85720dbcc4de3659e1ff292e89d9
egonlowe <ethan.lowe1304@gmail.com>
2025-11-15 14:49:04 -0500

    Update main.py w/ tests

--- src/main.py
+++ src/main.py
@@ -1,14 +1,36 @@
 from youtube_keyword_bot.data_loader import DataLoader
+from youtube_keyword_bot.text_processing import TextProcessing
 
-def main():
 
-    #test to ensure data is loaded and parsed correctly
-    loader = DataLoader("../data/ai_dataset.csv")
+def test_data_loader():
+    loader = DataLoader("data/ai_dataset.csv")
     dataset = loader.load_dataset()
 
     print(f"Loaded {len(dataset)} entries.")
     print("First entry:")
     print(dataset[0])
+
+
+def test_text_processing():
+    tp = TextProcessing()
+
+    tokens1 = tp.tokenize("sonic 3's subtle storytelling")
+    tokens2 = tp.tokenize("50 useless sonic heroes facts")
+
+    v1 = tp.vectorize(tokens1)
+    v2 = tp.vectorize(tokens2)
+
+    print("Tokens 1:", tokens1)
+    print("Tokens 2:", tokens2)
+    print("Similarity:", tp.similarity(v1, v2))
+
+
+def main():
+    # Enable one test at a time:
+
+    # test_data_loader()
+    test_text_processing()
+
 
 if __name__ == "__main__":
     main()
